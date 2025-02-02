79baab60ba222997f40b954fd34c0536e38c66b9
RisshiN24 <r.naavaal1@gmail.com>
2025-02-02 10:56:37 -0500

    Changed about page to interests page

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const About = () => {
+const Interests = () => {
   return (
     <div>
-      <h2>About Me</h2>
+      <h2>Interests</h2>
       <p>This is the About page. Here, I share more about myself and my hobbies!</p>
     </div>
   );
 };
 
-export default About;
+export default Interests;

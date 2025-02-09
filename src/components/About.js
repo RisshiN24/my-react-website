e1ba7ec749ee72b03f93f9bd367b81e8385475bd
RisshiN24 <r.naavaal1@gmail.com>
2025-02-08 21:18:36 -0500

    Added a banner photo to my about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './About.css';
+import bannerPhoto from '../assets/about-banner.jpg';
 
 const About = () => {
   return (
     <div className="about">
-      <h1 data-aos="fade" data-aos-delay="100">About Me!</h1>
+      <div className="banner-container">
+        <img src={bannerPhoto} alt="Risshi Naavaal" className="banner-photo" />
+        <div className="banner-overlay"></div>
+      </div>
+      <h1 data-aos="fade" data-aos-delay="100">About Me</h1>
       <p></p>
     </div>
   );
